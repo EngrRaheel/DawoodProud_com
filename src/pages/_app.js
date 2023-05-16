46ba94501c29c90b8141c9fd75f645c03bc02ed8
Engr. Raheel <engr.mraheeltariq@gmail.com>
2023-05-16 05:53:34 +0500

    all the issues resolved relteing to dependecies and tester

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
-<<<<<<< HEAD
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-=======
->>>>>>> 3b9991864c8f81d0f8bbcca843bf1b59022af316
+
 import WhatsAppFloatingButton from "../../components/WhatsAppFloatingButton";
 
 export default function App({ Component, pageProps }) {
